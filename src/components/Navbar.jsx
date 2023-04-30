2613dd8917eab87acf2e35e9c0ff61b1fcbfa08b
JacobJax <jacobjax617@gmail.com>
2023-04-30 12:50:15 +0300

    build navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import './Navbar.css'
+import Logo from '../assets/images/logo.svg'
+import Hamgurger from '../assets/images/icon-hamburger.svg'
+import { useState } from 'react'
+
+export default function Navbar() {
+   const [isOpen, setOpen] = useState(false)
+
+   function toggleOpen() {
+      setOpen(openState => {
+         return !openState
+      })
+   }
+
+   return(
+      <nav>
+         <div id='nav-container' className="container flex justify-between">
+            <div id="nav-logo" className='flex items-center'>
+               <img src={ Logo } alt="Sunny side logo" />
+            </div>
+            <div id="nav-links" className={isOpen ? 'md:w-2/4 nav-layer' : 'md:w-2/4'}>
+               <div id="menu" className='md:hidden'>
+                  <img src={ Hamgurger } alt="hamburger menu" onClick={ toggleOpen } />
+               </div>
+               <div id="menu-links" className='md:w-full md:flex md:justify-between'>
+                  <ul className='text-white md:flex md:justify-between md:w-3/4 md:items-center'>
+                     <li><a href="#">About</a></li>
+                     <li><a href="#">Services</a></li>
+                     <li><a href="#">Projects</a></li>
+                  </ul>
+                  <div className="btn-container">
+                     <a href="#" className="btn">CONTACT</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </nav>
+   )
+}
