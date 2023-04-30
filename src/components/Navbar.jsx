1ba2fbb42fd5d667130a8301afbe6c8c85c9e051
JacobJax <jacobjax617@gmail.com>
2023-04-30 12:55:11 +0300

    format navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,10 @@ export default function Navbar() {
                <img src={ Logo } alt="Sunny side logo" />
             </div>
             <div id="nav-links" className={isOpen ? 'md:w-2/4 nav-layer' : 'md:w-2/4'}>
-               <div id="menu" className='md:hidden'>
+               <div id="menu" className='md:hidden cursor-pointer'>
                   <img src={ Hamgurger } alt="hamburger menu" onClick={ toggleOpen } />
                </div>
-               <div id="menu-links" className='md:w-full md:flex md:justify-between'>
+               <div id="menu-links" className='md:w-full md:flex md:justify-between md:gap-4'>
                   <ul className='text-white md:flex md:justify-between md:w-3/4 md:items-center'>
                      <li><a href="#">About</a></li>
                      <li><a href="#">Services</a></li>
