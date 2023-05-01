2b01a4f40b7fd3a2d7d6f7b443958603ada0860c
Jacob Odero <jackjax617@gmail.com>
2023-05-01 12:59:46 +0300

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,13 +6,13 @@ export default function Services() {
          <div id="graphic" className="service text-center md:w-1/2">
             <div className="service-text">
                <h1 className='text-3xl'>Graphic Design</h1>
-               <p className='text-xl'>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.</p>
+               <p className='text-lg'>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.</p>
             </div>
          </div>
          <div id="photography" className="service text-center md:w-1/2">
             <div className="service-text">
                <h1 className='text-3xl'>Photography</h1>
-               <p className='text-xl'>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
+               <p className='text-lg'>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
             </div>
          </div>
       </section>
